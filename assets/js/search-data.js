977022544d98fa9c766bbb73368d4bbbc84ea122
vuqv <69011496+vuqv@users.noreply.github.com>
2025-06-24 04:47:42 +0000

    Deploying to gh-pages from @ obrien-lab/obrien-lab.github.io@02c3549a5ec30fcf42a3bd6c305a36cbb5cf2948 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,10 +440,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
@@ -451,7 +448,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-protein-accidentally-lassos-itself-helping-explain-unusual-refolding-behavior-sci-adv-paper",
+          title: 'Protein accidentally lassos itself, helping explain unusual refolding behavior (Sci. Adv paper)',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_1/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
